167b2493fcea211018acd9e5eca7e37a08a35eb1
Galina Zvoznikova <galya.zvoz@yandex.ru>
2024-12-23 21:21:34 +0300

    sprint 5

--- conftest.py
+++ conftest.py
@@ -5,10 +5,10 @@ from selenium import webdriver
 # Фикстура веб-драйвера
 @pytest.fixture(scope="function")
 def driver():
-    chrome_options = webdriver.FirefoxOptions()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--window-size=1920,1080')
     chrome_options.add_argument("--incognito")
-    driver = webdriver.Firefox(options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://stellarburgers.nomoreparties.site/")
     yield driver
     driver.quit()
